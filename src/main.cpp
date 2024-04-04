49dae79a5771e84eb3c7e476efed506a762fa299
DiegoStocco <diegostocco.2008@gmail.com>
2024-04-04 18:26:28 +0200

    Added the updating of the pendulum in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "double_pendulum.hpp"
+#include <glm/ext/vector_float2.hpp>
 #include <iostream>
 #include <window.h>
 #include <errors.h>
@@ -32,6 +34,30 @@ JAGE::Window* init()
 
 int main(int argc, char* argv[]) {
   JAGE::Window* window = init();
+
+  glm::vec2 masses({1.0, 1.0});
+  glm::vec2 lengths({1.0, 1.0});
+  glm::vec2 angles({1.5, 1.5});
+  glm::vec2 angular_velocity({0, 0});
+  DoublePendulum pendulum(masses, lengths, angles, angular_velocity);
+
+  double time = glfwGetTime();
+  double oldtime = glfwGetTime();
+  double deltaTime;
+  while(!window->shouldClose()) {
+    deltaTime = time - oldtime;
+
+    // Update pendulum's position and velocity
+    pendulum.update(deltaTime);
+
+    // TODO: render the pendulum, for now you can have the angles printed on stdout
+    std::cout << pendulum.getAngles().x << " " << pendulum.getAngles().y << std::endl;
+    
+    oldtime = time;
+    time = glfwGetTime();
+
+    glfwPollEvents();
+  }
 
   delete window;
 }
